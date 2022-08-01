a85cac66b2830c0112cf8681206ea017842c4595
Saadman Ahmed <1705068@ugrad.cse.buet.ac.bd>
2022-08-01 11:27:22 +0600

    minor bug fixed in dummy data json

--- frontend-client/dummy-data.js
+++ frontend-client/dummy-data.js
@@ -1,7 +1,7 @@
 const DUMMY_PROPERTIES = [
     {
       id: 'p1',
-      title: 'Programming for everyone',
+      title: 'Kazipara Smart Appartment Domino',
       description:
         'Everyone can learn to code! Yes, everyone! In this live event, we are going to go through all the key basics and get you started with programming as well.',
       location: 'Somestreet 25, 12345 San Somewhereo',
@@ -11,7 +11,7 @@ const DUMMY_PROPERTIES = [
     },
     {
       id: 'p2',
-      title: 'Networking for introverts',
+      title: 'Rightway Airport Plaza',
       description:
         "We know: Networking is no fun if you are an introvert person. That's why we came up with this event - it'll be so much easier. Promised!",
       location: 'New Wall Street 5, 98765 New Work',
@@ -21,14 +21,14 @@ const DUMMY_PROPERTIES = [
     },
     {
       id: 'p3',
-      title: 'Networking for extroverts',
+      title: 'Purbachal American City',
       description:
         'You probably need no help with networking in general. But focusing your energy correctly - that is something where most people can improve.',
       location: 'My Street 12, 10115 Broke City',
       date: '2022-04-10',
-      image: 'prop3.jpg',
+      image: 'images/prop3.jpg',
       isFeatured: true,
-    },
+    }
   ];
   
   export function getFeaturedEvents() {
