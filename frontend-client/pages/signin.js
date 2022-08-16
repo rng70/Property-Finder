5a6321e14716f5b1bb898f276e80ae8eba677666
Al Arafat Tanin <1705070@ugrad.cse.buet.ac.bd>
2022-08-16 14:53:14 +0600

    need to work on login & signup

--- frontend-client/pages/signin.js
+++ frontend-client/pages/signin.js
@@ -1,5 +1,68 @@
-const SingIn = () => {
-    return <div>Sign in</div>
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import React, { useState } from 'react';
+import { login } from '../actions/auth';
+import { Link, useNavigate } from 'react-router-dom';
+
+const Login = ({login, isAuthenticated}) => {
+
+    let navigation = useNavigate();
+    const [formData, setFormData] = useState({
+        email: '',
+        password: '',
+    });
+
+    const { email, password} = formData;
+    const onChange = e => setFormData({ ...formData, [e.target.name]: e.target.value });
+
+    const onSubmit = async e => {
+        e.preventDefault();
+        login(email, password);
+    }
+
+    if (isAuthenticated) {
+        navigation('/posts')
+    }
+    return (
+        <>
+            <h1 className="large text-primary">Sign In</h1>
+            <p className="lead"><i className="fas fa-user"></i> Sign In with Your Account</p>
+            <form className="form" onSubmit={e => onSubmit(e)}>
+                <div className="form-group">
+                    <input
+                        type="email"
+                        placeholder="Email Address"
+                        name="email"
+                        value={email}
+                        onChange={e => onChange(e)}
+                        required />
+                </div>
+                <div className="form-group">
+                <input
+                    type="password"
+                    placeholder="Password"
+                    name="password"
+                    value={password}
+                    onChange={e => onChange(e)}
+                    minLength="6"
+                />
+                </div>
+                <input type="submit" className="btn btn-primary" value="Login" />
+            </form>
+            <p className="my-1">
+                Don't have an account? <Link to="/Register">Sign Up</Link>
+            </p>
+        </>
+    )
 }
 
-export default SingIn;
+Login.propTypes = {
+    login: PropTypes.func.isRequired,
+    isAuthenticated: PropTypes.bool
+}
+
+const mapStateToProps = state => ({
+    isAuthenticated: state.auth.isAuthenticated
+})
+
+export default connect(mapStateToProps, {login})(Login);
