360393b6be6d6fa9800115b0500605f946719b5f
stebaratheon <1705068@ugrad.cse.buet.ac.bd>
2022-08-16 21:16:33 +0600

    Sign up and in type all completed

--- frontend-client/pages/_app.js
+++ frontend-client/pages/_app.js
@@ -1,9 +1,13 @@
 import '../styles/globals.css'
 import Layout from '../Components/layout/layout'
+// import { NavBar  } from '../Components' 
 
 function MyApp({ Component, pageProps }) {
   return (
+    <>
+    {/* <NavBar/> */}
     <Component {...pageProps} />
+    </>
   )
 }
 
