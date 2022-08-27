f9911773e32796445fa9953199f7db4cf6dec395
stebaratheon <1705068@ugrad.cse.buet.ac.bd>
2022-08-27 19:53:42 +0600

    Feed polished,Search bar added

--- client/src/components/feed/NewsFeed.js
+++ client/src/components/feed/NewsFeed.js
@@ -4,6 +4,9 @@ import { Jumbo } from '../';
 import { getLands,getHouses,getSpaces} from '../../actions/property';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import Select from 'react-select'
+import './style.css'
+import { useState } from 'react';
 
 const NewsFeed = ({ getLands, getHouses, getSpaces, lands, houses , spaces }) => {
   useEffect(() => {
@@ -12,19 +15,53 @@ const NewsFeed = ({ getLands, getHouses, getSpaces, lands, houses , spaces }) =>
     getSpaces();
   }, [getLands, getHouses, getSpaces]);
 
+  
   //later will be fetched from database
-  const description =
-    'See what are posted by our community to find your suitable place!';
-  //console.log("lands here ",lands);
+  const description = 'See what are posted by our community to find your suitable place!';
+  console.log("lands here ",lands);
   //console.log("houses here ",houses);
   //console.log("spaces herer ",spaces);
+
+
+  const [selectedArea, setSelectedArea] = useState();
+  const optionList = [
+    { area : "Dhaka", label :"Dhaka"},
+    { area : "Chattrogram", label :"Chattrogram"},
+    { area : "Rajshahi", label :"Rajshahi"},
+    { area : "Khulna", label :"Khulna"},
+    { area : "Barisal", label :"Barisal"},
+    { area : "Sylhet", label :"Sylhet"}
+  ];
+  const  handleSelect= (data)=> {
+    setSelectedArea(data);
+  }
+
+
+  const CardDivStyle = {
+    objectFit: "cover",
+    objectPosition: "0px 0px",
+    marginLeft : 5,
+    marginBottom : "5px",
+    marginTop : 5
+  }
   return (
     <div>
       <Jumbo description={description} />
+      <div className='search'>
+      <h2>Choose your Location</h2>
+            <div className='dropdown-container'>
+                <Select options={optionList} placeholder="Select Area"
+                value={selectedArea}
+                onChange={handleSelect}
+                isSearchable={true}/>
+            </div>
+      </div>
       <h2 className='mb-4'>Latest Posted</h2>
-      {lands.map((property, i) => {
-        return <Card key={i} property={property} />;
-      })}
+      <div className='row' style={CardDivStyle}>
+        {lands.map((property, i) => {
+          return <Card key={i} property={property} />;
+        })}
+      </div>
       <div>
         {houses.map((land, i) => {
           return (
